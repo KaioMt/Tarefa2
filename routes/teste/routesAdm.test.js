df1ffc17ac3ead5f012dbcd0fd795ca02733f0aa
KaioMt <morettokaio0@gmail.com>
2022-06-07 18:58:12 -0300

    Tarefa

--- routes/teste/routesAdm.test.js
+++ routes/teste/routesAdm.test.js
@@ -1,6 +1,6 @@
 const resquest =require('supertest');
 const app= require('../routesAdm');
-describe ('Teste de rota',()=>{
+describe ('Tarefa de rota',()=>{
     it("It should respond with an OK", async () => {
         const res=await resquest('http://localhost:3000').get('/index')
         expect(res.statusCode).toEqual(200)
